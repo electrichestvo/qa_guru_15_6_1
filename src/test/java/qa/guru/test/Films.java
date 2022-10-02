1da66381d2e024d440db8d12c7a99e912f1f5d70
mmslotin <balu54@yandex.ru>
2022-10-02 15:31:13 +0300

    New Tests added

---
+++ src/test/java/qa/guru/test/Films.java
@@ -0,0 +1,12 @@
+package qa.guru.test;
+
+public enum Films {
+
+    BREAKING_BAD("Во все тяжкие"), TOPI("Топи");
+
+    public final String desc;
+
+    Films(String desc) {
+        this.desc = desc;
+    }
+}
