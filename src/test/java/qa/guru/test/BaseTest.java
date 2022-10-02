818ec24ba59add74c205cd4308bd01332fdc6d9c
mmslotin <balu54@yandex.ru>
2022-10-02 15:26:02 +0300

    New Tests added

---
+++ src/test/java/qa/guru/test/BaseTest.java
@@ -0,0 +1,13 @@
+package qa.guru.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.kinopoisk.ru";
+        Configuration.browserSize = "1920x1080";
+    }
+}
